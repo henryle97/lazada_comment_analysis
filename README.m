d9b54b85a12fcffa576b0946bd2d01b7dfb10fd5
hoanglv <hoanglevan@admicro.vn>
2019-11-28 12:14:29 +0700

    big update

---
+++ README.m
@@ -0,0 +1,12 @@
+INSTALL:
+-Download embedding: https://drive.google.com/open?id=0B1GKSX6YCHXlMTVZNkFEYzRyd1E
+-Giải nén rồi lưu vào folder embedding
+- Run pip install -r requirements.txt
+
+TRAINING:
+Run main.py
+
+
+TODO:
+1. Thêm dấu cho từ
+2. Training
